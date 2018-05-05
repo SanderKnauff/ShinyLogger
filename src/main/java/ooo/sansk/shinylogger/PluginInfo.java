2aa9ae5e2f4623f03d4f9c1c6decd05a7b296eb1
Sansko1337 <sanderknauff@hotmail.com>
2018-05-05 23:39:49 +0200

    Add Gradle version to PluginInfo.java

--- src/main/java/ooo/sansk/shinylogger/PluginInfo.java
+++ src/main/java/ooo/sansk/shinylogger/PluginInfo.java
@@ -5,6 +5,7 @@ public class PluginInfo {
     public static final String PLUGIN_ID = "shinylogger";
     public static final String PLUGIN_NAME = "ShinyLogger";
     public static final String PLUGIN_DESCRIPTION = "Logs the amount of captured shiny pokemon per player";
+    public static final String PLUGIN_VERSION = "@version@";
 
     private PluginInfo() {
     }
